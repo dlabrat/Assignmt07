8a9dea9a94c9fd6bf9ff5d685f6d85d367bced49
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-04-21 19:53:57 -0700

    started

---
+++ js/script-array-practice.js
@@ -0,0 +1,56 @@
+//STEP 1
+/* let favMovies = ['Glory', 'Brave Heart', ' Wonder Woman', ' Rambo ', ' Spirited Away']
+for (let i of favMovies) {
+	console.log(i)					
+} */
+
+//STEP 2
+/* let myMovies = new Array(5)
+myMovies[0] = 'Glory'
+myMovies[1] = 'Brave Heart'
+myMovies[2] = 'Wonder Woman'
+myMovies[3] = 'Rambo'
+myMovies[4] = 'Spirited Away'
+console.log(myMovies[0]) */
+
+//STEP 3
+/* let myMovies = new Array(5)
+myMovies[0] = 'Glory'
+myMovies[1] = 'Brave Heart'
+myMovies[2] = 'Wonder Woman'
+myMovies[3] = 'Rambo'
+myMovies[4] = 'Spirited Away'
+//console.log(myMovies[0])
+myMovies.splice(2, 0, 'Disney\'s Robin Hood')
+console.log(myMovies.length) */
+
+//STEP 4
+let movies = ['Glory', 'Brave Heart', ' Wonder Woman', ' Rambo ', ' Spirited Away']
+delete movies[0]               
+console.log(movies)
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
