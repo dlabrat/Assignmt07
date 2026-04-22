5c5daf6d6acb818bf113cf7914f3fbb50d93a347
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-04-21 21:26:25 -0700

    so far..

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -25,19 +25,62 @@ myMovies.splice(2, 0, 'Disney\'s Robin Hood')
 console.log(myMovies.length) */
 
 //STEP 4
-let movies = ['Glory', 'Brave Heart', ' Wonder Woman', ' Rambo ', ' Spirited Away']
+/* let movies = ['Glory', 'Brave Heart', ' Wonder Woman', ' Rambo ', ' Spirited Away']
 delete movies[0]               
-console.log(movies)
+console.log(movies) */
 
 //STEP 5
-
+/* let movies =[]
+movies = ['Glory', 'Brave Heart', 'Wonder Woman', 'Rambo ', 'Spirited Away', 'A Shot in the Dark', 'Jurassic Park']
+//use for-in loop to display
+for (let movie in movies) {
+	console.log(movies[movie])
+}
+ */
 //STEP 6
+/* let movies =[]
+movies = ['Glory', 'Brave Heart', 'Wonder Woman', 'Rambo ', 'Spirited Away', 'A Shot in the Dark', 'Jurassic Park'] 
+//use for-of loop to display
+for (let i of movies) {
+	console.log(i)	
+} */
 
 //STEP 7
+/* let movies =[]
+movies = ['Glory', 'Brave Heart', 'Wonder Woman', 'Rambo ', 'Spirited Away', 'A Shot in the Dark', 'Jurassic Park'] 
+for (let i of movies.sort()) {
+	console.log(i)	
+} */
 
 //STEP 8
+/* let movies =[]
+movies = ['Glory', 'Brave Heart', 'Wonder Woman', 'Rambo ', 'Spirited Away', 'A Shot in the Dark', 'Jurassic Park'] 
+console.log('Movies I like: \n')
+for (let movie in movies) {
+	console.log(movies[movie])
+}
+let leastFavMovies =[]
+leastFavMovies[0] = 'Coctail'
+leastFavMovies[1] = 'Scary Movie'
+leastFavMovies[2] = 'The Smurfs'
+console.log('\nMovies I regret watching: \n')
+for (let movie in leastFavMovies) {
+	console.log(leastFavMovies[movie])
+} */
 
 //STEP 9
+let fmovies =[]
+fmovies = ['Glory', 'Brave Heart', 'Wonder Woman', 'Rambo ', 'Spirited Away', 'A Shot in the Dark', 'Jurassic Park'] 
+let leastFavMovies =[]
+leastFavMovies[0] = 'Coctail'
+leastFavMovies[1] = 'Scary Movie'
+leastFavMovies[2] = 'The Smurfs'
+//join with concat 
+let movies = fmovies.concat(leastFavMovies)
+console.log('All movies: \n')
+//display reverse sorted 
+movies = movies.sort()
+console.log(movies.reverse())
 
 //STEP 10
 
